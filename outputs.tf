249a6611d02296ae90472d742c3f1c536bcd4216
Brad Sickles <brad.sickles@gmail.com>
2021-08-10 21:31:42 -0400

    Emitting secret_id instead of secret_name for admin

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "db_admin_secret_name" {
-  value       = aws_secretsmanager_secret.auth_token.name
-  description = "string ||| The name of the secret in AWS Secrets Manager containing the auth token"
+output "db_admin_secret_id" {
+  value       = aws_secretsmanager_secret.auth_token.id
+  description = "string ||| The ID of the secret in AWS Secrets Manager containing the auth token"
 }
 
 output "db_protocol" {
