2060aafe84cd0c5c2079fdba4118392f60f62173
Brad Sickles <brad.sickles@gmail.com>
2022-01-06 22:49:37 -0500

    Removing cluster mode, setting db protocol when tls is enabled

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "db_admin_secret_id" {
 }
 
 output "db_protocol" {
-  value       = "redis"
+  value       = aws_elasticache_replication_group.this.transit_encryption_enabled ? "rediss" : "redis"
   description = "string ||| This emits `redis` and is used for generalized data store contracts."
 }
 
@@ -14,17 +14,11 @@ locals {
 }
 
 output "db_endpoint" {
-  // There are 2 different endpoints exported based on whether cluster mode is enabled
-  value       = var.node_group_count > 0 ? local.cluster_endpoint : local.single_endpoint
+  value       = local.single_endpoint
   description = "string ||| The endpoint URL to access Redis."
 }
 
 output "db_security_group_id" {
   value       = aws_security_group.this.id
   description = "string ||| The ID of the security group attached to Redis."
-}
-
-output "is_tls_enabled" {
-  value       = aws_elasticache_replication_group.this.transit_encryption_enabled
-  description = "bool ||| Describes whether transit encryption is enabled for Redis."
 }
--- redis.tf
+++ redis.tf
@@ -1,24 +1,23 @@
 resource "aws_elasticache_cluster" "this" {
   cluster_id           = local.resource_name
   replication_group_id = aws_elasticache_replication_group.this.id
+  tags                 = local.tags
 }
 
 locals {
-  port          = 6379
-  is_multi_node = var.node_count > 1
-  cluster_mode  = var.node_group_count > 0 ? [{ groups = var.node_group_count, replicas = var.replicas_per_node }] : []
+  port = 6379
 }
 
 resource "aws_elasticache_subnet_group" "this" {
   name       = local.resource_name
   subnet_ids = local.private_subnet_ids
-  tags       = data.ns_workspace.this.tags
+  tags       = local.tags
 }
 
 resource "aws_elasticache_replication_group" "this" {
   replication_group_id          = local.resource_name
   replication_group_description = "${local.resource_name} - Managed via Nullstone"
-  tags                          = data.ns_workspace.this.tags
+  tags                          = local.tags
   auto_minor_version_upgrade    = true
   at_rest_encryption_enabled    = true
   transit_encryption_enabled    = true
@@ -29,17 +28,8 @@ resource "aws_elasticache_replication_group" "this" {
   port                          = local.port
   security_group_ids            = [aws_security_group.this.id]
   subnet_group_name             = aws_elasticache_subnet_group.this.name
-  multi_az_enabled              = length(local.private_subnet_ids) > 1 && local.is_multi_node
-  automatic_failover_enabled    = local.is_multi_node
-  number_cache_clusters         = var.node_count
+  multi_az_enabled              = false
+  automatic_failover_enabled    = false
+  number_cache_clusters         = 2
   apply_immediately             = true
-
-  dynamic "cluster_mode" {
-    for_each = local.cluster_mode
-
-    content {
-      num_node_groups         = cluster_mode.value.groups
-      replicas_per_node_group = cluster_mode.value.replicas
-    }
-  }
 }
