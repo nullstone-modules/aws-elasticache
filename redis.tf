f533cddae7aa04af0c098f6ab2f02bb3a7c536cf
Brad Sickles <brad.sickles@gmail.com>
2022-02-10 14:39:36 -0500

    Update aws tf provider

--- redis.tf
+++ redis.tf
@@ -15,21 +15,21 @@ resource "aws_elasticache_subnet_group" "this" {
 }
 
 resource "aws_elasticache_replication_group" "this" {
-  replication_group_id          = local.resource_name
-  replication_group_description = "${local.resource_name} - Managed via Nullstone"
-  tags                          = local.tags
-  auto_minor_version_upgrade    = true
-  at_rest_encryption_enabled    = true
-  transit_encryption_enabled    = true
-  auth_token                    = random_password.auth_token.result
-  engine                        = "redis"
-  engine_version                = var.redis_version
-  node_type                     = var.node_type
-  port                          = local.port
-  security_group_ids            = [aws_security_group.this.id]
-  subnet_group_name             = aws_elasticache_subnet_group.this.name
-  multi_az_enabled              = false
-  automatic_failover_enabled    = false
-  number_cache_clusters         = 2
-  apply_immediately             = true
+  replication_group_id       = local.resource_name
+  description                = "${local.resource_name} - Managed via Nullstone"
+  tags                       = local.tags
+  auto_minor_version_upgrade = true
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  auth_token                 = random_password.auth_token.result
+  engine                     = "redis"
+  engine_version             = var.redis_version
+  node_type                  = var.node_type
+  port                       = local.port
+  security_group_ids         = [aws_security_group.this.id]
+  subnet_group_name          = aws_elasticache_subnet_group.this.name
+  multi_az_enabled           = false
+  automatic_failover_enabled = false
+  num_cache_clusters         = 2
+  apply_immediately          = true
 }
