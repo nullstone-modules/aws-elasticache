50e0853e002669c688c011f39e18ba5f1bbb08ad
Brad Sickles <brad.sickles@gmail.com>
2022-02-22 21:44:21 -0500

    Added module manifest

--- nullstone.tf
+++ nullstone.tf
@@ -8,13 +8,27 @@ terraform {
 
 data "ns_workspace" "this" {}
 
+// Generate a random suffix to ensure uniqueness of resources
+resource "random_string" "resource_suffix" {
+  length  = 5
+  lower   = true
+  upper   = false
+  number  = false
+  special = false
+}
+
+locals {
+  tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
+  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
+}
+
 data "ns_connection" "network" {
   name = "network"
   type = "network/aws"
 }
 
 locals {
-  tags               = data.ns_workspace.this.tags
   vpc_id             = data.ns_connection.network.outputs.vpc_id
   private_subnet_ids = data.ns_connection.network.outputs.private_subnet_ids
 }
