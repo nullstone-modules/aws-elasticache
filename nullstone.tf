825f9ef32c828031a9fc59081716cbd3cc7e1b68
Brad Sickles <brad.sickles@gmail.com>
2022-06-11 09:13:46 -0400

    contract

--- nullstone.tf
+++ nullstone.tf
@@ -22,13 +22,3 @@ locals {
   block_name    = data.ns_workspace.this.block_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
-
-data "ns_connection" "network" {
-  name = "network"
-  type = "network/aws"
-}
-
-locals {
-  vpc_id             = data.ns_connection.network.outputs.vpc_id
-  private_subnet_ids = data.ns_connection.network.outputs.private_subnet_ids
-}
